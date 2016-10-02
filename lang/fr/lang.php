2c43868e6588d1eeaa6ff975c6616cf4109a77c1
cziehr <christoph@christoph-ziehr.de>
2016-10-02 14:23:07 +0200

    Create lang.php

---
+++ lang/fr/lang.php
@@ -0,0 +1,9 @@
+<?php
+/*
+french language file
+ 
+%d will be replaced with a link to the destination.
+%s will be replaced with the number of seconds before redirect.
+ */
+$lang['redirect']="Vous allez être redirigé vers la page %d dans approximativement %s secondes";
+?>
