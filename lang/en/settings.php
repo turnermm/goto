b4c2fae58312fcc1f9937e5d3989f2f89fcf53d7
cziehr <christoph@christoph-ziehr.de>
2016-10-02 15:39:56 +0200

    Create settings.php

---
+++ lang/en/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 3 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author cziehr <info@einsatzleiterwiki.de>
+ */
+$lang['seconds']     = 'Time till redirection (in seconds)';
+$lang['minSeconds']  = 'Minimal time which is allowed till redirection (in seconds)';
