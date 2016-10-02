ce2359894c420f0af7a4009b54037ad4c7346cd9
cziehr <christoph@christoph-ziehr.de>
2016-10-02 15:23:58 +0200

    Create metadata.php

---
+++ conf/metadata.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Options for the goto plugin
+ *
+ * @author cziehr <info@einsatzleiterwiki.de>
+ */
+
+$meta['seconds']       = array('numeric', '_pattern' => '/[0-9]+/');
+$meta['minSeconds']    = array('numeric', '_pattern' => '/[0-9]+/');
