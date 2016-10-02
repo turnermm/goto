50b49521e83cc68dbd758d0d8c91e8144bc4397f
cziehr <christoph@christoph-ziehr.de>
2016-10-02 15:35:45 +0200

    Create settings.php

---
+++ lang/de/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 3 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author cziehr <info@einsatzleiterwiki.de>
+ */
+$lang['seconds']     = 'Standard-Zeit, bis zur verwiesenen Seite weitergeleitet wird';
+$lang['minSeconds']  = 'Minimale Zeit, nach der auf die verwiesene Seite weitergeleitet wird (zur Beschränkung bei manueller Festlegung der Weiterleitungs-Zeit)';
