acdf834ac43226e64abe152b58c47d7a4e229049
cziehr <christoph@christoph-ziehr.de>
2016-10-02 14:18:48 +0200

    Create lang.php

---
+++ lang/en/lang.php
@@ -0,0 +1,9 @@
+<?php
+/*
+English language file
+ 
+%d will be replaced with a link to the destination.
+%s will be replaced with the number of seconds before redirect.
+ */
+$lang['redirect']="You will be redirected to %d in approximately %s seconds.";
+?>
