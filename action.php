3748bb08418bae064d8f7a40f62ad600e947bfb7
Myron Turner <turnermm02@shaw.ca>
2019-03-20 17:07:52 -0500

    Refactoring event hook for auto-login to prepare for groups

--- action.php
+++ action.php
@@ -8,7 +8,36 @@ if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'action.php');
 class action_plugin_goto extends DokuWiki_Action_Plugin {
     public function register(Doku_Event_Handler $controller) {   
-      $controller->register_hook('AUTH_LOGIN_CHECK', 'BEFORE', $this, 'handle_login',array('before'));
+	  $controller->register_hook('ACTION_ACT_PREPROCESS', 'BEFORE', $this, 'handle_act',array('before'));   
+    }
+	
+	function handle_act(Doku_Event $event, $param) {   	   
+	   global $conf,$USERINFO,$INPUT;
+		$act = act_clean($event->data);
+		 if($act != 'login') {
+				return;
+		}
+	    $user = $_SERVER['REMOTE_USER'];
+		if(!$user) return;
+		$grps = $USERINFO['grps'];		
+		$auto_login = $this->getConf('auto_login');   
+		
+		if($auto_login) {		       
+		   $option  = $this->getConf('auto_options');
+		   $common = $this->getConf('common_ns');
+		   if($common) {
+			   $common = rtrim($common,':');    
+		   }               
+		   $srch = array('common_ns','user_page','user_ns','start_page');               
+		   $repl = array($common,$user,$user,$conf['start']);
+		   $value = str_replace($srch,$repl,$option); 
+		   setcookie("GOTO_LOGIN", $value, time()+120, DOKU_BASE);
+		   return;
+		}
+		else {
+			setcookie("DOKU_GOTO", $event->data['user'], time()+120, DOKU_BASE);
+		}
+		
     }
 	function handle_login(Doku_Event $event, $param) {   
 	    global $conf;
