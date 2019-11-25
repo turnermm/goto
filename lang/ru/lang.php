09967e4d8f7cee91b31add0794243c32b7ed0d7a
Щербинин Андрей Владимирович <ScherbininAV@hq.corp.mos.ru>
2019-11-25 06:23:20 +0300

    Russian localization
     * Almost all rows translated (WIP)

---
+++ lang/ru/lang.php
@@ -0,0 +1,8 @@
+<?php
+/*
+English language file
+
+%d will be replaced with a link to the destination.
+%s will be replaced with the number of seconds before redirect.
+ */
+$lang['redirect']="Вы будете перенаправлены на страницу %d примерно через %s секунд.";
